aabde0041e9fba786681c51bdb1b28922de43ddd
Nyfong <litongfong12@gmail.com>
2025-05-01 21:47:32 +0700

    docker compos setupv1

--- config/database.php
+++ config/database.php
@@ -1,6 +1,8 @@
 <?php
 class Database {
-    private $host = "my-postgres"; // service name in docker-compose
+    // Change from my-postgres to the service name in docker-compose.yml
+    // Change from my-postgres to the service name in docker-compose.yml
+    private $host = "my-postgres"; // It should be the service name
     private $db_name = "php_test";
     private $username = "postgres";
     private $password = "7cc01d92c8dce52eaf441dbae694cb34858e55ebeac43afbf100cb71de287b6c";
